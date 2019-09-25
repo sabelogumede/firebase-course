3041c6b8247932a82d0ae5db983600a280cd9e19
sabelogumede <iamsabelogumede@gmail.com>
2019-09-25 13:05:14 +0200

    querying a firestore Collection using angularFire

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,26 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import * as firebase from 'firebase/app';
-import 'firebase/firestore';
-// course model interface
-import { Course } from '../model/course';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyD5EEBzLtHAWPovZcLu3n0q6r0msrRlSh8",
-  authDomain: "fir-course-f970f.firebaseapp.com",
-  databaseURL: "https://fir-course-f970f.firebaseio.com",
-  projectId: "fir-course-f970f",
-  storageBucket: "fir-course-f970f.appspot.com",
-  messagingSenderId: "501260201422",
-  appId: "1:501260201422:web:2e2755048920b398d40f9f",
-  measurementId: "G-S0NTEG8SP5"
-};
-// passing our config to the sdk using the firebase object
-firebase.initializeApp(firebaseConfig);
-// firestore db refrence
-const db = firebase.firestore();
-//
-// const settings = {timestampsInSnapshots: true};
-// db.settings(settings);
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'about',
@@ -29,31 +8,14 @@ const db = firebase.firestore();
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private db: AngularFirestore) { }
 
   ngOnInit() {
-    // db object used to query firestore doc'
-    // it takes the path & uniqe id ref of each doc
-    // gets and returns a promise with a doc snapshort
-
-    // db.doc('courses/3cnonA5KPtFzxz3y2zGy')
-    //     .get()
-    //     .then(snap => console.log(snap.data()));
-
-    db.collection('courses')
-        .get()
-        .then(snaps => {
-
-          const courses: Course[] = snaps.docs.map(snap => {
-              return <Course>{
-                id: snap.id,
-                ...snap.data()
-              };
-            });
-
-          console.log(courses);
-
-        });
+    // query the collection by name
+    // valueChanges returns an Observable that we subscribe to
+    // valueChanges observable gives us  a live connection to the firestore database
+    this.db.collection('courses').valueChanges()
+      .subscribe(val => console.log(val));
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,13 @@
 export const environment = {
   production: false,
   firebase: {
-
+    apiKey: "AIzaSyD5EEBzLtHAWPovZcLu3n0q6r0msrRlSh8",
+    authDomain: "fir-course-f970f.firebaseapp.com",
+    databaseURL: "https://fir-course-f970f.firebaseio.com",
+    projectId: "fir-course-f970f",
+    storageBucket: "fir-course-f970f.appspot.com",
+    messagingSenderId: "501260201422",
+    appId: "1:501260201422:web:2e2755048920b398d40f9f",
+    measurementId: "G-S0NTEG8SP5"
   }
 };
