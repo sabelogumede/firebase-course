675b509a26c8af895ba945715fe60b9a1a92c4fa
sabelogumede <iamsabelogumede@gmail.com>
2019-09-25 10:05:23 +0200

    firebase-course project

---
+++ init-db.ts
@@ -0,0 +1,89 @@
+
+import {COURSES, findLessonsForCourse} from './db-data';
+
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyD5EEBzLtHAWPovZcLu3n0q6r0msrRlSh8",
+  authDomain: "fir-course-f970f.firebaseapp.com",
+  databaseURL: "https://fir-course-f970f.firebaseio.com",
+  projectId: "fir-course-f970f",
+  storageBucket: "fir-course-f970f.appspot.com",
+  messagingSenderId: "501260201422",
+  appId: "1:501260201422:web:2e2755048920b398d40f9f",
+  measurementId: "G-S0NTEG8SP5"
+};
+
+console.log("Uploading data to the database with the following config:\n");
+
+console.log(JSON.stringify(config));
+
+console.log("\n\n\n\nMake sure that this is your own database, so that you have write access to it.\n\n\n");
+
+firebase.initializeApp(config);
+
+const db = firebase.firestore();
+
+async function uploadData() {
+
+  var batch = db.batch();
+
+  const courses = db.collection('courses');
+
+
+  Object.values(COURSES)
+    .sort((c1:any, c2:any) => c1.seqNo - c2.seqNo)
+    .forEach(async (course:any) => {
+
+      const newCourse = removeId(course);
+
+      const courseRef = await courses.add(newCourse);
+
+      const lessons = courseRef.collection("lessons");
+
+      const courseLessons = findLessonsForCourse(course.id);
+
+      //console.log(`Adding ${courseLessons.length} lessons to ${course.description}`);
+
+      courseLessons.forEach(async lesson => {
+
+        const newLesson = removeId(lesson);
+
+        await lessons.add(newLesson);
+
+      });
+
+    });
+
+  return batch.commit();
+}
+
+
+function removeId(data:any) {
+
+  const newData: any = {...data};
+
+  delete newData.id;
+
+  return newData;
+}
+
+
+uploadData()
+  .then(() => {
+    console.log("Writing data, exiting in 10 seconds ...\n\n");
+
+    setTimeout(() => {
+
+      console.log("\n\n\nData Upload Completed.\n\n\n");
+      process.exit(0);
+
+    }, 10000);
+
+  })
+  .catch(err => {
+    console.log("Data upload failed, reason:", err, '\n\n\n');
+    process.exit(-1);
+  });
+
+
